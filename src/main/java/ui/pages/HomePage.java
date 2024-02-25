4ebe16572d3fb3f2a255d0938437890145f478e8
serhi_u <serhiyudovychenko@gmail.com>
2024-02-25 14:22:28 +0100

    Initial commit message

--- src/main/java/ui/pages/HomePage.java
+++ src/main/java/ui/pages/HomePage.java
@@ -3,25 +3,18 @@ package ui.pages;
 import com.epam.jdi.light.elements.common.UIElement;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.XPath;
-import com.epam.jdi.light.ui.html.elements.common.Button;
-
-import static utils.PageWaiters.waitForJStoLoad;
 
 @Url("")
 public class HomePage extends WebPage {
+    @XPath("//ul[@role='menu']//a[contains(@href, '/gear.html')]")
+    private UIElement gearCategory;
+    @XPath("//ul[@role='menu']//a[contains(@href, '/gear/bags.html')]")
+    private UIElement bagsSubCategory;
 
-    @Css(".global-nav__branding-logo")
-    private UIElement homePageLogo;
-
-    @XPath("//*[@class='global-nav__me-photo evi-image ghost-person ember-view']")
-    private Button myAccountIcon;
-
-    public void verifyHomePageOpened() {
-        waitForJStoLoad();
-        homePageLogo.shouldBe().displayed();
-        myAccountIcon.shouldBe().displayed();
+    public void openBagsSubcategory() {
+        gearCategory.hover();
+        bagsSubCategory.click();
     }
 
 }
